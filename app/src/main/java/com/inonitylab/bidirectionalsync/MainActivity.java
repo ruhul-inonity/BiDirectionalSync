1b778df8df77936d9dda47e987a2d2e88fdd0853
“ruhul-inonity” <ruhul.azam1@gmail.com>
2016-12-28 17:59:05 +0600

    receiving data from server and informing server

--- app/src/main/java/com/inonitylab/bidirectionalsync/MainActivity.java
+++ app/src/main/java/com/inonitylab/bidirectionalsync/MainActivity.java
@@ -1,22 +1,80 @@
 package com.inonitylab.bidirectionalsync;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    // DB Class to perform DB related operations
+    DBController controller = new DBController(this);
+    // Progress Dialog Object
+    ProgressDialog prgDialog;
+    HashMap<String, String> queryValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Get User records from SQLite DB
+        ArrayList<HashMap<String, String>> userList = controller.getAllUsers();
+        // If users exists in SQLite DB
+        if (userList.size() != 0) {
+            // Set the User Array list in ListView
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, userList, R.layout.view_user_entry, new String[] {
+                    "userId", "userName" }, new int[] { R.id.userId, R.id.userName });
+            ListView myList = (ListView) findViewById(android.R.id.list);
+            myList.setAdapter(adapter);
+        }
+        // Initialize Progress Dialog properties
+        prgDialog = new ProgressDialog(this);
+        prgDialog.setMessage("Transferring Data from Remote MySQL DB and Syncing SQLite. Please wait...");
+        prgDialog.setCancelable(false);
+        // BroadCase Receiver Intent Object
+        Intent alarmIntent = new Intent(getApplicationContext(), SampleBC.class);
+        // Pending Intent Object
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Alarm Manager Object
+        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        // Alarm Manager calls BroadCast for every Ten seconds (10 * 1000), BroadCase further calls service to check if new records are inserted in
+        // Remote MySQL DB
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis() + 5000, 10 * 1000, pendingIntent);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +104,122 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id == R.id.action_refresh) {
+            syncSQLiteMySQLDB();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Method to Sync MySQL to SQLite DB
+    public void syncSQLiteMySQLDB() {
+        // Create AsycHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Http Request Params Object
+        RequestParams params = new RequestParams();
+        // Show ProgressBar
+        prgDialog.show();
+        // Make Http call to getusers.php
+        client.post("http://api.inonity.com/BiDirectionalSync/getusers.php", params, new BaseJsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
+                prgDialog.hide();
+                // Update SQLite DB with response sent by getusers.php
+               // String response = bytes.toString();
+                Log.d("Main onSuccess","....................... "+ " rawJsonResponses "+rawJsonResponse+" obj res"+response);
+                updateSQLite(rawJsonResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
+
+                prgDialog.hide();
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet]",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+                return null;
+            }
+        });
+    }
+
+    public void updateSQLite(String response){
+        ArrayList<HashMap<String, String>> usersynclist;
+        usersynclist = new ArrayList<HashMap<String, String>>();
+        // Create GSON object
+        Gson gson = new GsonBuilder().create();
+        try {
+            // Extract JSON array from the response
+            JSONArray arr = new JSONArray(response);
+            System.out.println(arr.length());
+            // If no of array elements is not zero
+            if(arr.length() != 0){
+                // Loop through each array element, get JSON object which has userid and username
+                for (int i = 0; i < arr.length(); i++) {
+                    // Get JSON object
+                    JSONObject obj = (JSONObject) arr.get(i);
+
+                    Log.d("userId ","................... "+obj.get("userId")+"  userName"+obj.get("userName"));
+                    // DB QueryValues Object to insert into SQLite
+                    queryValues = new HashMap<String, String>();
+                    // Add userID extracted from Object
+                    queryValues.put("userId", obj.get("userId").toString());
+                    // Add userName extracted from Object
+                    queryValues.put("userName", obj.get("userName").toString());
+                    // Insert User into SQLite DB
+                    controller.insertUser(queryValues);
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    // Add status for each User in Hashmap
+                    map.put("Id", obj.get("userId").toString());
+                    map.put("status", "1");
+                    usersynclist.add(map);
+                }
+                // Inform Remote MySQL DB about the completion of Sync activity by passing Sync status of Users
+                updateMySQLSyncSts(gson.toJson(usersynclist));
+                // Reload the Main Activity
+                reloadActivity();
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    // Method to inform remote MySQL DB about completion of Sync activity
+    public void updateMySQLSyncSts(String json) {
+        Log.d("Main act updateMySql","...................... Gson made json"+json);
+        System.out.println(json);
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("syncsts", json);
+        // Make Http call to updatesyncsts.php with JSON parameter which has Sync statuses of Users
+        client.post("http://api.inonity.com/BiDirectionalSync/updatesyncsts.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] response) {
+                Toast.makeText(getApplicationContext(),	"MySQL DB has been informed about Sync activity", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] response, Throwable throwable) {
+                Toast.makeText(getApplicationContext(),	"MySQL DB has been informed about Sync activity", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    // Reload MainActivity
+    public void reloadActivity() {
+        Intent objIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(objIntent);
     }
 }
---
+++ app/src/main/java/com/inonitylab/bidirectionalsync/SampleBC.java
@@ -0,0 +1,78 @@
+package com.inonitylab.bidirectionalsync;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.BaseJsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created by ruhul on 12/28/16.
+ */
+
+public class SampleBC extends BroadcastReceiver {
+    static int noOfTimes = 0;
+
+    // Method gets called when Broad Case is issued from MainActivity for every 10 seconds
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        noOfTimes++;
+        Toast.makeText(context, "BC Service Running for " + noOfTimes + " times", Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        // Checks if new records are inserted in Remote MySQL DB to proceed with Sync operation
+        client.post("http://api.inonity.com/BiDirectionalSync/getdbrowcount.php",params ,new BaseJsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String rawJsonResponse, Object response) {
+                Log.d("Sample BC on receive"," response ......................... "+rawJsonResponse);
+                try {
+                    // Create JSON object out of the response sent by getdbrowcount.php
+                    JSONObject obj = new JSONObject(rawJsonResponse);
+                    System.out.println(obj.get("count"));
+                    // If the count value is not zero, call MyService to display notification
+                    if(obj.getInt("count") != 0){
+                        final Intent intnt = new Intent(context, MyService.class);
+                        // Set unsynced count in intent data
+                        intnt.putExtra("intntdata", "Unsynced Rows Count "+obj.getInt("count"));
+                        // Call MyService
+                        context.startService(intnt);
+                    }else{
+                        Toast.makeText(context, "Sync not needed", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, String rawJsonData, Object errorResponse) {
+
+                // TODO Auto-generated method stub
+                if(statusCode == 404){
+                    Toast.makeText(context, "404", Toast.LENGTH_SHORT).show();
+                }else if(statusCode == 500){
+                    Toast.makeText(context, "500", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(context, "Error occured!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            protected Object parseResponse(String rawJsonData, boolean isFailure) throws Throwable {
+                return null;
+            }
+
+        });
+    }
+}
